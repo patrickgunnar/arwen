05f4b4832722278feb02b89471afe386caf896fe
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-14 20:02:52 -0300

    feat: added routes pages.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Label from "@/src/components/label/Label";
+import Link from "next/link";
 
 export default async function Home() {
     return (
@@ -6,9 +7,15 @@ export default async function Home() {
             <h4 className="text-3xl">
                 <Label name="Home" value="title" />
             </h4>
-            <p>
+            <p className="mb-10">
                 <Label name="Home" value="description" />
             </p>
+            <Link href="/system/home">System Home</Link>
+            <Link href="/system/home/overview">Overview</Link>
+            <Link href="/system/home/announcements">Announcements</Link>
+            <Link href="/system/home/updates">Updates</Link>
+            <Link href="/system/home/getting-started">Getting Started</Link>
+            <Link href=""></Link>
         </div>
     );
 }
