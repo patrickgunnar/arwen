608d232f8a550d56146835fff9e40458ef7b1560
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-17 20:53:28 -0300

    feat: added search modal.

--- src/app/(office)/_components/navbar/Search.tsx
+++ src/app/(office)/_components/navbar/Search.tsx
@@ -1,59 +1,58 @@
 "use client";
 
-import { Button } from "@/src/components/ui/button";
+import Hint from "@/src/components/hit/Hint";
 import { Input } from "@/src/components/ui/input";
-import { ChevronDown, SearchIcon, X } from "lucide-react";
+import { useTranslations } from "@/src/hooks/useTranslations";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import SearchModal from "./SearchModal";
+import { X } from "lucide-react";
 
 export default function Search() {
     const pathname = usePathname();
     const pathnameArr = pathname.split("/").filter((path) => path.length > 0);
+    const label = useTranslations({
+        page: "Home",
+        label: "search",
+    });
+    const histLabel = useTranslations({
+        page: "Home",
+        label: "advancedSearch",
+    });
 
+    const [value, setValue] = useState<string>("");
     const [searchIn, setSearchIn] = useState<string>(
         pathnameArr[pathnameArr.length - 1] ?? ""
     );
-    const [isAdvanceSearch, setIsAdvanceSearch] = useState<boolean>(false);
-    const [value, setValue] = useState<string>("");
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {};
 
     const onClear = () => setValue("");
 
     return (
-        <div>
-            <form
-                className="relative flex items-center w-full lg:w-[300px]"
-                onSubmit={onSubmit}
-            >
-                <Button
-                    type="button"
-                    size="default"
-                    variant="transparentYellow"
-                    onClick={() => setIsAdvanceSearch((prev) => !prev)}
-                    className="flex gap-x-1 items-center justify-center bg-white/85 border border-r-0 border-secondary rounded-r-none"
-                >
-                    <SearchIcon className="text-secondary-foreground h-4 w-4" />
-                    <ChevronDown className="text-secondary-foreground h-4 w-4" />
-                </Button>
-                <Input
-                    value={value}
-                    onChange={(ev) => setValue(ev.target.value)}
-                    placeholder={`Search in /${searchIn}`}
-                    className="font-light text-black bg-white/85 placeholder:text-secondary-foreground border border-l-0 border-secondary pr-10 rounded-l-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
-                />
-                {value && (
-                    <div className="absolute flex items-center justify-center right-3 h-full">
-                        <X
-                            className="text-secondary-foreground cursor-pointer transition h-4 w-6 hover:opacity-75"
-                            onClick={onClear}
-                        />
-                    </div>
-                )}
-            </form>
-            {isAdvanceSearch && (
-                <div className="absolute text-black">Advance Search</div>
+        <form
+            className="relative flex items-center w-full lg:w-[300px]"
+            onSubmit={onSubmit}
+        >
+            <Hint align="end" side="bottom" label={histLabel} asChild>
+                <div>
+                    <SearchModal />
+                </div>
+            </Hint>
+            <Input
+                value={value}
+                onChange={(ev) => setValue(ev.target.value)}
+                placeholder={`${label} /${searchIn}`}
+                className="font-light text-black bg-white/85 placeholder:text-secondary-foreground border border-l-0 border-secondary pr-10 rounded-l-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
+            />
+            {value && (
+                <div className="absolute flex items-center justify-center right-3 h-full">
+                    <X
+                        className="text-secondary-foreground cursor-pointer transition h-4 w-6 hover:opacity-75"
+                        onClick={onClear}
+                    />
+                </div>
             )}
-        </div>
+        </form>
     );
 }
