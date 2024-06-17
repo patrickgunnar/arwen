2d707ab692105bf730f6a4565209fc4297a28b34
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-17 20:53:00 -0300

    feat: added history modal.

--- src/app/(office)/_components/navbar/History.tsx
+++ src/app/(office)/_components/navbar/History.tsx
@@ -1,24 +1,21 @@
 "use client";
 
-import { Button } from "@/src/components/ui/button";
-import { History as HistoryIcon } from "lucide-react";
-import { useState } from "react";
+import Hint from "@/src/components/hit/Hint";
+import Label from "@/src/components/label/Label";
+import HistoryModal from "./HistoryModal";
 
 export default function History() {
-    const [isOpened, setIsOpened] = useState<boolean>(false);
+    const TextElement = () => {
+        return <Label name="Home" value="history" />;
+    };
 
     return (
-        <div>
-            <div className="flex items-center justify-center p-2">
-                <Button
-                    variant="transparentYellow"
-                    onClick={() => setIsOpened((prev) => !prev)}
-                    className="relative"
-                >
-                    <HistoryIcon className="text-secondary-foreground h-6 w-6" />
-                </Button>
-            </div>
-            {isOpened && <div className="absolute text-black">History</div>}
+        <div className="flex items-center justify-center p-2">
+            <Hint align="end" textElement={TextElement} side="bottom" asChild>
+                <div>
+                    <HistoryModal />
+                </div>
+            </Hint>
         </div>
     );
 }
