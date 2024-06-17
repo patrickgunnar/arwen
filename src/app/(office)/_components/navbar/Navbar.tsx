7d3a54426c1e1e5fd82347e5964ba7a66aef7f73
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-17 15:45:47 -0300

    feat: navbar component.

---
+++ src/app/(office)/_components/navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Container from "./Container";
+import History from "./History";
+import MobileNav from "./MobileNav";
+import Organizations from "./Organizations";
+import Search from "./Search";
+
+export default function Navbar() {
+    return (
+        <div className="flex items-center justify-between bg-foreground text-secondary-foreground p-1 h-14 overflow-hidden">
+            <Container>
+                <div className="hidden md:block">
+                    <History />
+                </div>
+                <Search />
+            </Container>
+            <div className="hidden lg:block">
+                <Container>
+                    <Organizations />
+                    <div className="mx-1">|</div>
+                    <Container>
+                        <div>A</div>
+                        <div>B</div>
+                        <div>C</div>
+                        <div>D</div>
+                        <div>E</div>
+                        <div>F</div>
+                    </Container>
+                </Container>
+            </div>
+            <div className="lg:hidden">
+                <MobileNav />
+            </div>
+        </div>
+    );
+}
