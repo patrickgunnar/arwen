1e97b2976195c1b530c49ad03da01dd9c2c51d25
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-19 20:54:05 -0300

    feat: mobile navigation component.

--- src/app/(office)/_components/navbar/MobileNav.tsx
+++ src/app/(office)/_components/navbar/MobileNav.tsx
@@ -1,25 +1,11 @@
 "use client";
 
-import { Button } from "@/src/components/ui/button";
-import { AlignJustify } from "lucide-react";
-import { useState } from "react";
+import MobileNavModal from "./MobileNavModal";
 
 export default function MobileNav() {
-    const [isOpened, setIsOpened] = useState<boolean>(false);
-
     return (
         <div>
-            <div className="flex items-center justify-center ml-4 mr-1">
-                <Button
-                    variant="transparentYellow"
-                    onClick={() => setIsOpened((prev) => !prev)}
-                >
-                    <AlignJustify className="text-secondary-foreground h-5 w-5" />
-                </Button>
-            </div>
-            {isOpened && (
-                <div className="absolute text-black">Mobile Navigation</div>
-            )}
+            <MobileNavModal />
         </div>
     );
 }
---
+++ src/app/(office)/_components/navbar/MobileNavModal.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogClose,
+} from "@/src/components/ui/dialog";
+import { Button } from "@/src/components/ui/button";
+import { AlignJustify, Cog } from "lucide-react";
+import { useTranslations } from "@/src/hooks/useTranslations";
+import CreateModal from "./CreateModal";
+import ReferAndEarnModal from "./ReferAndEarnModal";
+import NotificationsModal from "./NotificationsModal";
+import AppsModal from "./AppsModal";
+import Link from "next/link";
+import OrganizationsModal from "./OrganizationsModal";
+import { useMediaQuery } from "usehooks-ts";
+import { ElementRef, useEffect, useRef } from "react";
+
+export default function MobileNavModal() {
+    const closeRef = useRef<ElementRef<"button">>(null);
+    const matches = useMediaQuery("(min-width: 1280px)");
+
+    const organizationName = "Arwen's org";
+    const title = useTranslations({
+        page: "System",
+        label: "mobileNav",
+        nested: "Navbar",
+    });
+
+    const createTitle = useTranslations({
+        page: "System",
+        label: "create",
+        nested: "Navbar",
+    });
+    const referAndEarnTitle = useTranslations({
+        page: "System",
+        label: "refer&earn",
+        nested: "Navbar",
+    });
+    const notificationsTitle = useTranslations({
+        page: "System",
+        label: "notifications",
+        nested: "Navbar",
+    });
+    const settingsTitle = useTranslations({
+        page: "System",
+        label: "settings",
+        nested: "Navbar",
+    });
+    const appsTitle = useTranslations({
+        page: "System",
+        label: "apps",
+        nested: "Navbar",
+    });
+
+    useEffect(() => {
+        if (matches) {
+            closeRef.current?.click();
+        }
+    }, [matches]);
+
+    return (
+        <Dialog>
+            <DialogClose ref={closeRef}></DialogClose>
+            <DialogTrigger asChild>
+                <Button variant="transparentYellow">
+                    <AlignJustify className="text-secondary-foreground h-5 w-5" />
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="bg-primary border border-gray-900 text-white xl:hidden">
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                </DialogHeader>
+                <div className="w-full">
+                    <div>
+                        <CreateModal buttonTitle={createTitle} />
+                        <ReferAndEarnModal buttonTitle={referAndEarnTitle} />
+                        <NotificationsModal buttonTitle={notificationsTitle} />
+                        <Button
+                            size="lg"
+                            variant="transparentYellow"
+                            className="flex items-center justify-center text-white w-full"
+                        >
+                            <Link
+                                href="/system/settings"
+                                className="flex gap-x-2 items-center justify-start h-full w-full"
+                                onClick={() => {
+                                    closeRef.current?.click();
+                                }}
+                            >
+                                <Cog className="h-6 w-6" />
+                                <p>{settingsTitle}</p>
+                            </Link>
+                        </Button>
+                        <AppsModal buttonTitle={appsTitle} />
+                    </div>
+                    <div className="mt-6 w-full">
+                        <OrganizationsModal
+                            organizationName={organizationName}
+                            isMobileNav
+                        />
+                    </div>
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
+}
