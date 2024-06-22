21639a34d044fdf9f61d06363b6ad5be1d00b8be
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-22 19:05:48 -0300

    feat: added memoization.

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import { useTranslations } from "@/src/hooks/useTranslations";
 
 interface LabelProps {
@@ -8,7 +9,7 @@ interface LabelProps {
     nested?: string;
 }
 
-export default function Label({ name, value, nested = "" }: LabelProps) {
+function Label({ name, value, nested = "" }: LabelProps) {
     const label = useTranslations({
         page: name,
         label: value,
@@ -17,3 +18,5 @@ export default function Label({ name, value, nested = "" }: LabelProps) {
 
     return <>{label}</>;
 }
+
+export default React.memo(Label);
