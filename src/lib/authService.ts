c48a2070272a8fdc3e5856115fcaeb44db43b895
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-25 20:52:28 -0300

    feat: added getSelf handler; it collects the current user.

---
+++ src/lib/authService.ts
@@ -0,0 +1,28 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "./db";
+
+export async function getSelf() {
+    try {
+        const self = await currentUser();
+
+        if (!self || !self.username) {
+            throw new Error("Unauthorized");
+        }
+
+        const user = db.user.findUnique({
+            where: {
+                externalUserId: self.id,
+            },
+        });
+
+        if (!user) {
+            throw new Error("User not found!");
+        }
+
+        return user;
+    } catch (error: any) {
+        console.error(error);
+
+        return null;
+    }
+}
