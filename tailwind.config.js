083f09b92eae9f4738f89525c1b348e9b172e54d
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-24 23:41:28 -0300

    feat: added plugin.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,28 @@ module.exports = {
     ],
     prefix: "",
     theme: {
+        patterns: {
+            opacities: {
+                100: "1",
+                80: ".80",
+                60: ".60",
+                40: ".40",
+                20: ".20",
+                10: ".10",
+                5: ".05",
+            },
+            sizes: {
+                1: "0.25rem",
+                2: "0.5rem",
+                4: "1rem",
+                6: "1.5rem",
+                8: "2rem",
+                16: "4rem",
+                20: "5rem",
+                24: "6rem",
+                32: "8rem",
+            },
+        },
         container: {
             center: true,
             padding: "2rem",
@@ -18,7 +40,7 @@ module.exports = {
         },
         extend: {
             colors: {
-                "darkened": "hsl(var(--darkened))",
+                darkened: "hsl(var(--darkened))",
                 yellow01: "hsl(var(--yellow-01))",
                 "primary-with-transparency": {
                     DEFAULT: "hsla(var(--primary-with-transparency))",
@@ -86,5 +108,8 @@ module.exports = {
             },
         },
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [
+        require("tailwindcss-animate"),
+        require("tailwindcss-bg-patterns"),
+    ],
 };
