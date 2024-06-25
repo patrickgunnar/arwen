ec199e5cb6b1338a7d84f6d44925ab1ca4920f42
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-24 23:42:03 -0300

    feat: added background pattern.

--- src/app/(office)/system/home/layout.tsx
+++ src/app/(office)/system/home/layout.tsx
@@ -7,10 +7,11 @@ export default function HomeLayout({ children }: { children: string }) {
 
     return (
         <>
-            <div className="flex flex-col gap-y-8 bg-white rounded-t-md rounded-b-none p-2 pb-0 w-full">
+            <div className="relative flex flex-col gap-y-8 bg-white rounded-t-md rounded-b-none p-2 pb-0 w-full overflow-hidden">
+                <div className="absolute pattern-wavy pattern-yellow-500 pattern-bg-white pattern-size-8 pattern-opacity-40 top-0 left-0 h-full w-full"></div>
                 <HomeHeader username={username} orgsName={orgsName} />
                 <HomeNavigation />
-                <div className="lg:hidden h-10 w-full">
+                <div className="lg:hidden z-50 h-10 w-full">
                     {/* TODO: Home Navigation Mobile */}
                 </div>
             </div>
