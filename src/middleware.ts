bb79c4d7873bb45c8f2b79d3aaa5569971a21b29
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-25 20:49:35 -0300

    refactor: improved middleware.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,45 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/system(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+    "/system",
+    "/system/documents",
+    "/system/integrations",
+    "/system/reports",
+    "/system/settings",
+    "/system/home",
+    "/system/home/overview",
+    "/system/home/announcements",
+    "/system/home/getting-started",
+    "/system/home/recent-updates",
+    "/system/inventory",
+    "/system/inventory/adjustments",
+    "/system/inventory/groups",
+    "/system/inventory/items",
+    "/system/purchases",
+    "/system/purchases/bills",
+    "/system/purchases/expenses",
+    "/system/purchases/payment-made",
+    "/system/purchases/purchase-orders",
+    "/system/purchases/purchase-receives",
+    "/system/purchases/vendor-credits",
+    "/system/purchases/vendors",
+    "/system/sales",
+    "/system/sales/customers",
+    "/system/sales/credit-notes",
+    "/system/sales/invoices",
+    "/system/sales/packages",
+    "/system/sales/payment-received",
+    "/system/sales/sales-orders",
+    "/system/sales/sales-receipt",
+    "/system/sales/sales-returns",
+    "/system/sales/shipments",
+    "/system(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
     if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
-    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
