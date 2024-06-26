090f4d1d909271580866aef7e94ca0e6990d9cc1
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-25 21:37:03 -0300

    feat: now is collecting the current user.

--- src/app/(office)/layout.tsx
+++ src/app/(office)/layout.tsx
@@ -1,20 +1,33 @@
+import UserProvider from "@/src/components/providers/UserProvider";
 import ContentTable from "./_components/ContentTable";
 import MainContent from "./_components/MainContent";
 import Navbar from "./_components/navbar/Navbar";
 import Sidebar from "./_components/sidebar/Sidebar";
+import { getSelf } from "@/src/lib/authService";
+import { UserType } from "@/src/store/slices/user";
 
-export default function OfficeLayout({
+export default async function OfficeLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const self = await getSelf();
+    const user = { ...self };
+
+    if (user) {
+        delete user.externalUserId;
+        delete user.id;
+    }
+
     return (
-        <div className="flex bg-background text-inherit h-full w-full overflow-hidden">
-            <Sidebar />
-            <ContentTable>
-                <Navbar />
-                <MainContent>{children}</MainContent>
-            </ContentTable>
-        </div>
+        <UserProvider user={user as UserType}>
+            <div className="flex bg-background text-inherit h-full w-full overflow-hidden">
+                <Sidebar />
+                <ContentTable>
+                    <Navbar />
+                    <MainContent>{children}</MainContent>
+                </ContentTable>
+            </div>
+        </UserProvider>
     );
 }
