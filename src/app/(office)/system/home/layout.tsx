02cbea24f65b532db385ab969cbe86ef68323a0e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-25 21:37:23 -0300

    refactor: removed hard coded values.

--- src/app/(office)/system/home/layout.tsx
+++ src/app/(office)/system/home/layout.tsx
@@ -2,14 +2,11 @@ import HomeNavigation from "./_components/HomeNavigation";
 import HomeHeader from "./_components/HomeHeader";
 
 export default function HomeLayout({ children }: { children: string }) {
-    const orgsName = "Arwen's org";
-    const username = "Arwen";
-
     return (
         <>
             <div className="relative flex flex-col gap-y-8 bg-white rounded-t-md rounded-b-none p-2 pb-0 w-full overflow-hidden">
                 <div className="absolute pattern-wavy pattern-yellow-500 pattern-bg-white pattern-size-8 pattern-opacity-40 top-0 left-0 h-full w-full"></div>
-                <HomeHeader username={username} orgsName={orgsName} />
+                <HomeHeader />
                 <HomeNavigation />
                 <div className="lg:hidden z-50 h-10 w-full">
                     {/* TODO: Home Navigation Mobile */}
