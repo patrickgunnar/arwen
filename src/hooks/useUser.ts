f74409f41358923e92cd9c7cce6b66910360c7f2
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-25 21:40:08 -0300

    refactor: removed unnecessary useMemo hook.

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -2,13 +2,8 @@
 
 import { useSelector } from "react-redux";
 import { StoreType } from "../store";
-import { useMemo } from "react";
 import { UserType } from "../store/slices/user";
 
 export function useUser(): UserType {
-    const user = useSelector((state: StoreType) => state.user);
-
-    return useMemo(() => {
-        return user;
-    }, [user]);
+    return useSelector((state: StoreType) => state.user);
 }
