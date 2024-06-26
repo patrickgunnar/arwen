0006cc700d4d53819ee864357d5389c8cd201a6e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-06-26 18:31:47 -0300

    feat: added mobile navigation conditions and styles.

--- src/app/(office)/system/home/_components/NavigationItem.tsx
+++ src/app/(office)/system/home/_components/NavigationItem.tsx
@@ -6,24 +6,40 @@ import { Button } from "@/src/components/ui/button";
 import Link from "next/link";
 import { cn } from "@/src/lib/utils";
 
-export function NavigationItem({ title, path, icon: Icon }: HomeRoutes) {
+export function NavigationItem({
+    title,
+    path,
+    icon: Icon,
+    isMobileNav,
+    onClick,
+}: HomeRoutes & { isMobileNav?: boolean; onClick?: () => void }) {
     const pathname = usePathname();
 
     return (
-        <li>
+        <li
+            className={cn({
+                "w-full": isMobileNav,
+            })}
+        >
             <Button
                 variant="ghost"
                 className={cn(
                     "border-b-2 rounded-none h-full w-full hover:bg-transparent hover:border-yellow-400",
                     {
                         "border-transparent": pathname !== path,
-                        "border-yellow-500 pointer-events-none": pathname === path,
+                        "border-yellow-500 pointer-events-none":
+                            pathname === path,
+                        "hover:text-white": isMobileNav,
                     }
                 )}
             >
                 <Link
                     href={path}
-                    className="flex items-center justify-center gap-x-2 h-full w-full"
+                    className={cn("flex items-center gap-x-2 h-full w-full", {
+                        "justify-center": !isMobileNav,
+                        "justify-start": isMobileNav,
+                    })}
+                    onClick={onClick}
                 >
                     {Icon && <Icon className="h-5 w-5" />}
                     {title}
